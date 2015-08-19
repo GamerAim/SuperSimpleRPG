354523b504eb799bd00532b9bb3aea99b71bf372
Brandc <Gate2TheStars@gmail.com>
2015-08-18 18:40:43 -0700

    Starting map loading code

--- src/LoadMap.c
+++ src/LoadMap.c
@@ -70,9 +70,15 @@ typedef struct {
 }objectGroup_t;
 
 typedef struct {
-	/* The height and width present in this structure may not reflect each
-	 * tileSet
-	 */
+	/*Height and width in cells of map*/
+	int width;
+	int height;
+
+	/*Pixel length of each tile*/
+	int tileWidth;
+	int tileHeight;
+
+	/*Contains surface info for tiles and individual tile info*/
 	int tileSetsLen;
 	tileSet_t *tileSets;
 
@@ -85,7 +91,8 @@ typedef struct {
 	mapLayer_t *layers;
 
 	/*Objects*/
-	
+	int objectGroupsLen;
+	objectGroup_t *objectGroups;
 }map_t;
 
 /* Map format (%d means integer, and %s means string):
@@ -134,13 +141,255 @@ typedef struct {
  * Lastly, a line break and EOF
  */
 
-map_t *LoadMap()
+
+
+/*local def*/
+#define MAXNUMLEN 32
+#define MAXSTRINGLEN 128
+
+int getNum(char *data, int *offset)
 {
-	return NULL;
+	int i, ret;
+
+	ret = 0;
+	for (i = (*offset); i < MAXNUMLEN; i++) {
+		if (data[i] < '0' || data[i] > '9')
+			break;
+		else {
+			ret *= 10;
+			ret += data[i] - '0';
+		}
+	}
+
+	if (data[i] != ',' || data[i] != '\n')
+		return -1;
+
+	*offset = i;
+	return ret;
 }
 
+char *getString(char *data, int *offset)
+{
+	char *ret;
+	int i, len;
+
+	for (i = (*offset); i < MAXSTRINGLEN; i++)
+		if (data[i] == '\'')
+			break;
+	len = i - (*offset);
+	if (len <= 0)
+		return NULL;
+
+	ret = calloc(len+1, sizeof(char));
+	if (ret == NULL)
+		return NULL;
+
+	strncpy(ret, data+(*offset), len);
+
+	*offset = i;
+	return ret;
+}
+
+map_t *LoadMap(char *name)
+{
+	int i;
+	int tmp;
+	char *map;
+	char *num;
+	map_t *ret;
+	int offset;
+	int64_t mapLen;
+	SDL_RWops *file;
+
+	/*Open file and load into memory*/
+	file = SDL_RWFromFile(name, "rb");
+	if (file == NULL)
+		die("Failed to open file %s because %s\n", SDL_GetError());
+
+	mapLen = SDL_RWsize(file);
+	if (mapLen < 0) {
+		if (SDL_RWclose(file) < 0)
+			die("Failed to close file because %s\n", SDL_GetError());
+		return NULL;
+	}
+	else if (mapLen == 0) {
+		if (SDL_RWclose(file) < 0)
+			die("Failed to close file because %s\n", SDL_GetError());
+
+		/*Zero length files are invalid for this format.*/
+		return NULL;
+	}
+
+	map = calloc(mapLen, sizeof(char));
+	if (map == NULL) {
+		if (SDL_RWclose(file) < 0)
+			die("Failed to close file because %s\n", SDL_GetError());
+		return NULL;
+	}
+
+	if (SDL_RWread(file, map, sizeof(char), mapLen) != mapLen) {
+		free(map);
+		if (SDL_RWclose(file) < 0)
+			die("Failed to close file because %s\n", SDL_GetError());
+		return NULL;
+	}
+
+	if (SDL_RWclose(file) < 0)
+		die("Failed to close file because %s\n", SDL_GetError());
 
 
+	/*Initialize variables, to shut gcc up*/
+	num    = NULL;
+	ret    = NULL;
+	offset = 0;
 
 
+	/*Start the memory allocation process for return variables*/
+	num = calloc(MAXNUMLEN, sizeof(char));
+	if (num == NULL)  {
+		free(map);
+		return NULL;
+	}
+	ret = calloc(sizeof(map_t), 1);
+	if (ret == NULL)  {
+		free(map);
+		free(num);
+		return NULL;
+	}
 
+
+	/*Parse header*/
+	offset += 1;
+	ret->width  = getNum(map, &offset);
+	if (ret->width < 0)
+		goto headererr;
+	offset += 2;
+
+	ret->height = getNum(map, &offset);
+	if (ret->height < 0)
+		goto headererr;
+	offset += 3;
+
+	ret->tileWidth = getNum(map, &offset);
+	if (ret->tileWidth < 0)
+		goto headererr;
+	offset += 3;
+
+	ret->tileHeight = getNum(map, &offset);
+	if (ret->tileHeight < 0)
+		goto headererr;
+	offset += 1;
+
+
+	/*Parse tileset stuff*/
+	offset += 1;
+	ret->tileSetsLen = getNum(map, &offset);
+	if (ret->tileSetsLen < 0)
+		goto headererr;
+	else if (ret->tileSetsLen == 0)
+		goto headererr;
+	offset += 1;
+
+	ret->tileSets = calloc(sizeof(tileSet_t), ret->tileSetsLen);
+	if (ret->tileSets == NULL)
+		goto headererr;
+
+	/*Load tile sets into memory*/
+	for (i = 0; i < ret->tileSetsLen; i++) {
+		offset++;
+		tmp = getNum(map, &offset);
+		if (tmp < 0)
+			goto tileseterr;
+		ret->tileSets[i].imageWidth = tmp;
+		offset += 2;
+
+		tmp = getNum(map, &offset);
+		if (tmp < 0)
+			goto tileseterr;
+		ret->tileSets[i].imageHeight = tmp;
+		offset += 3;
+
+		tmp = getNum(map, &offset);
+		if (tmp < 0)
+			goto tileseterr;
+		ret->tileSets[i].tileWidth = tmp;
+		offset += 3;
+
+		tmp = getNum(map, &offset);
+		if (tmp < 0)
+			goto tileseterr;
+		ret->tileSets[i].tileHeight = tmp;
+		offset += 2;
+
+		tmp = getNum(map, &offset);
+		if (tmp < 0)
+			goto tileseterr;
+		ret->tileSets[i].startingGID = tmp;
+		offset += 3;
+
+		ret->tileSets[i].name = getString(map, &offset);
+		if (ret->tileSets[i].name == NULL)
+			goto tileseterr;
+		offset += 1;
+
+		char fpath[MAXSTRINGLEN*2];
+		snprintf(fpath, MAXSTRINGLEN*2, "assets/%s", \
+			 ret->tileSets[i].name);
+		ret->tileSets[i].image = IMG_Load(fpath);
+		if (ret->tileSets[i].image == NULL)
+			goto tileseterr;
+		offset += 2;
+	}
+/*
+ * Next line: "l%d"
+ * 	l = amount of layers
+ * 
+ * Following lines are equal to the number of layers:
+ * 	Formated as "w%d,h%d,d%d,%d,%d,%d,...":
+ * 		w = width in cells
+ * 		h = height in cells
+ * 		d = gid values equal to w*h, seperated by a comma as, but not
+ * 		    ending or starting with one. Example:
+ * 			%d,%d,%d,%d
+*/
+	/*Now to fetch the map layers*/
+	offset++;
+	ret->layersLen = getNum(map, &offset);
+//	if (getNum)
+//		goto tileseterr;
+//	offset++;
+
+//	ret->layers = calloc(sizeof(mapLayer_t), ret->layersLen);
+//	if (ret->layers == NULL)
+//		goto tileseterr;
+/*
+	for (i = 0; i < ret->layersLen; i++) {
+		
+	}
+*/
+
+
+	/*Clean up and return*/
+	free(map);
+	free(num);
+	return ret;
+
+
+	/*error section of code*/
+	tileseterr:
+		if (ret->tileSetsLen < 0)
+			goto headererr;
+		for (i = 0; i < ret->tileSetsLen; i++) {
+			if (ret->tileSets[i].name != NULL)
+				free(ret->tileSets[i].name);
+			if (ret->tileSets[i].image != NULL)
+				SDL_FreeSurface(ret->tileSets[i].image);
+		}
+	headererr:
+		free(map);
+		free(num);
+		free(ret);
+		return NULL;
+}
+
+#undef MAXNUMLEN
--- src/main.c
+++ src/main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdarg.h>
+#include <string.h>
 
 /*Official Third party libs*/
 #include "SDL2/SDL.h"
